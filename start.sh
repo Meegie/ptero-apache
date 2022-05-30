16191be431b8ac045e4bb1fcb834fd8c2a11f196
BasToTheMax <bas@arnold-linda.nl>
2022-05-30 17:30:27 +0200

    Try to fix

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 #!/bin/ash
 
+echo "Checking Apache config..."
+/usr/sbin/httpd -t
+
+sleep 3
+
 echo "Starting Apache..."
-/usr/sbin/httpd -f /home/container/apache/apache2.conf
+/usr/sbin/httpd -f /home/container/apache/apache2.conf --with-mpm=core.c
